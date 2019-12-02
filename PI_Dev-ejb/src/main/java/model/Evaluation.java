fefb037140a30394f7a5360e4a5b91a961ae0771
Dorra mahouachi <dorra.mahouachi@esprit.tn>
2019-12-03 00:27:38 +0100

    Classe Eval+Critere

---
+++ PI_Dev-ejb/src/main/java/model/Evaluation.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the Evaluations database table.
+ * 
+ */
+@Entity
+@Table(name="Evaluations")
+@NamedQuery(name="Evaluation.findAll", query="SELECT e FROM Evaluation e")
+public class Evaluation implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="EvaluationID")
+	private int evaluationID;
+
+	@Column(name="Description")
+	private String description;
+
+	//bi-directional one-to-one association to Candidature
+	@OneToOne
+	@JoinColumn(name="candidatureId")
+	private Candidature candidature;
+
+	public Evaluation() {
+	}
+
+	public int getEvaluationID() {
+		return this.evaluationID;
+	}
+
+	public void setEvaluationID(int evaluationID) {
+		this.evaluationID = evaluationID;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Candidature getCandidature() {
+		return this.candidature;
+	}
+
+	public void setCandidature(Candidature candidature) {
+		this.candidature = candidature;
+	}
+
+}
